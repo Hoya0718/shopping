93cf58966fedccd454925e61aba9900a9b74e279
TakHoya <130663193+Hoya0718@users.noreply.github.com>
2024-12-10 21:06:48 +0900

    db Connercion

--- src/main/java/com/nayo/shop/BasicController.java
+++ src/main/java/com/nayo/shop/BasicController.java
@@ -1,14 +1,15 @@
 package com.nayo.shop;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // Controller + ResponseBody를 결합
+@Controller // Controller + ResponseBody를 결합
 public class BasicController {
 
     @GetMapping( "/") //URL로 접속 시 return 데이터를 응답
     String hello() {
-        return "index.html";
+        return "redirect:index.html";
     }
 }
