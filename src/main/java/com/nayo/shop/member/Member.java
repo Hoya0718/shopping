f0f6d691fa53f574bb240d8779451898b6b9dcb1
TakHoya <130663193+Hoya0718@users.noreply.github.com>
2024-12-15 19:46:38 +0900

    @Many to One 구현 2024-12-15 19:46

--- src/main/java/com/nayo/shop/member/Member.java
+++ src/main/java/com/nayo/shop/member/Member.java
@@ -1,8 +1,12 @@
 package com.nayo.shop.member;
 
+import com.nayo.shop.sales.Sales;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @ToString
@@ -15,4 +19,8 @@ public class Member {
     private String username;
     private String password;
     private String displayName;
+
+    @ToString.Exclude //순환참조 방지
+    @OneToMany(mappedBy ="member") //member테이블의 참조 컬럼들
+    private List<Sales> sales = new ArrayList<>();
 }
--- src/main/java/com/nayo/shop/member/MemberDTO.java
+++ src/main/java/com/nayo/shop/member/MemberDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class MemberDTO {
+    private Integer id;
     private String username;
     private String password;
     private String displayName;
